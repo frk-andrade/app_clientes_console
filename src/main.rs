0c8dba17845410b7e05ba65b2a6dcba496df659a
Franklin Andrade <100174287+frk-andrade@users.noreply.github.com>
2026-07-02 16:06:57 -0300

    divisão dos módulos e estrutura básica de um cliente

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod models;
+
+use models::cliente::Cliente;
+
+use crate::models::cliente;
+
+
 fn main() {
-    println!("Hello, world!");
+    let cliente: Cliente = Cliente{
+        id: 1,
+        nome: "teste".to_string(),
+        cpf: "12345678900".to_string(),
+        endereco: "Rua teste".to_string(),
+    };
+
+    println!("Cliente: {}", cliente.nome);
 }
